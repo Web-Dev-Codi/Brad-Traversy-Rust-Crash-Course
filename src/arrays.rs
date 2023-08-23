b917beff729117a71769c9b0bd2814a85cd94db3
Web-Dev-Codi <cordiscobrian@gmail.com>
2023-08-23 14:01:23 +0200

    Arrays

---
+++ src/arrays.rs
@@ -0,0 +1,21 @@
+// Arrays are a fixed size and need to be all the same data type.
+use std::mem;
+
+pub fn run() {
+    // Must be staticly typed and the number of elements the array contains
+    let mut numbers: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // Re-assign a value
+    numbers[2] = 40;
+    // TO print full Array
+    println!("{:?}", numbers);
+
+    // Get single value
+    println!("Single Value: {}", numbers[2]);
+
+    // Get Array length
+    println!("Array length: {}", numbers.len());
+
+    // Arrays are stack allocated.
+    println!("Array occupies {} bytes", mem::size_of_val(&numbers));
+}
--- src/tuples.rs
+++ src/tuples.rs
@@ -1,4 +1,12 @@
 // Tuples group together values of a different types.
 // Max 12 elements
 
-pub fn run() {}
+pub fn run() {
+    // Types must be predefined
+    let person: (&str, &str, i8) = ("Gülden", "Berlin, Germany", 34);
+
+    println!(
+        "{0} is from {1} and is {2} years old.",
+        person.0, person.1, person.2
+    );
+}
