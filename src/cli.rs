1640552f8d38b64f769155c9eca90d9ab5f078fa
Web-Dev-Codi <cordiscobrian@gmail.com>
2023-08-25 15:56:08 +0200

    Course Complete

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,16 @@
-use std::env
+use std::env;
 
 pub fn run() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<String> = env::args().collect();
+    let command = args[1].clone();
+    let name = "Gülden";
+    let status = "100%";
+
+    if command == "hello" {
+        println!("Hi {}, How is your day going?", name);
+    } else if command == "status" {
+        println!("Status: {}", status);
+    } else {
+        println!("Not a valid command");
+    }
 }
